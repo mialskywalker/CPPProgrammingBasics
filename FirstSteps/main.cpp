b726daa2c6d6abafaa73cab4735ec551cf81b3f0
mialskywalker <kalqga123@gmail.com>
2024-01-06 18:12:36 +0200

    pet shop

--- FirstSteps/main.cpp
+++ FirstSteps/main.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main() {
-    string firstName;
+    const double DOG_FOOD = 2.5;
+    const double CAT_FOOD = 4.0;
 
-    cin >> firstName;
-    cout << "Hello, " << firstName << "!";
+    int dogPackets = 0;
+    int catPackets = 0;
+    double result = 0.0;
+
+    cin >> dogPackets >> catPackets;
+    result = (dogPackets * DOG_FOOD) + (catPackets * CAT_FOOD);
+
+    cout << result << " lv.";
 }
